b15164d6406560c142248036ddb89f29a2206c5d
Steven Buckingham <steven.buckingham@ucl.ac.uk>
2018-07-12 15:52:19 +0100

    started working on unit testing

--- mosquito/wsgi.py
+++ mosquito/wsgi.py
@@ -8,7 +8,8 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-
+import django
+django.setup()
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mosquito.settings")
